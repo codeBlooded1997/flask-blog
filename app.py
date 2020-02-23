5292ba6bd9e9d723f94dd418339406c9451ae16e
codeBlooded1997 <52186148+codeBlooded1997@users.noreply.github.com>
2020-02-22 19:44:34 -0500

    posts.html Updated

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
-
 app = Flask(__name__)  # created app
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///posts.db"  # configuring setting required for database and defining which database are we using
-db = SQLAlchemy(app)    # links the app and database to eachother
+db = SQLAlchemy(app)  # links the app and database to eachother
+
 
 # MODEL
-class BlogPost(db.Model):
+class BlogPosts(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     content = db.Column(db.Text, nullable=False)
@@ -19,32 +19,33 @@ class BlogPost(db.Model):
     def __repr__(self):
         return 'Blog post ' + str(self.id)
 
-
 all_posts = [
     {'title': 'post1',
      'content': 'This is content of post 1. lalalala....',
      'author': 'arian'
-    },
+     },
     {'title': 'post2',
      'content': 'This is content of post 2. hahaha....'
-    }
+     }
 ]
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html')    # sending to front-end
 
-@app.route('/posts')
+@app.route('/posts', methods=['GET', 'POST'])
 def posts():
-    return render_template('posts.html', posts=all_posts)
+    if request.method == 'Post':
+        post_title = request.form['title']
+        post_content = request.form['contetnt']
+        new_post = BlogPosts(title=post_title, content=post_content, author='Kareyo')   # creating postst from inputs to save in db
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        all_posts = BlogPosts.query.order_by(BlogPosts.date_posted).all()   # reading posts form db
+        return render_template('posts.html', posts=all_posts)   # sending to front-end
 
-@app.route('/home/users/<string:name>/posts/<int:id>')  # defining a route
-def hello(name, id):  # this code will run whenever we get to the URL in previous line
-    return "Hello, " + name + ", your id is : " + str(id)
-
-@app.route('/onlyget', methods=['POST'])
-def get_req():
-    return "You can only get this webopage 4."
 
 if __name__ == "__main__":  # if we are running the app from command line(__main__)
     app.run(debug=True)  # it will turn on debug mode
